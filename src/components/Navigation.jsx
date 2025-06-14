fefe2d8332e7c1d81f2a5f05a69df9214ea09a9c
Brittany Joiner <brittanyjoiner15@gmail.com>
2025-06-14 17:25:07 -0500

    add logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import { NavLink, Link } from 'react-router-dom'
 import { useState } from 'react'
+import ttlogo from '../assets/tt-light.svg'
 
 function Navigation() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -7,7 +8,10 @@ function Navigation() {
         <nav className="main-nav">
             <div className="nav-container">
                 <div className="nav-content">
-                    <NavLink to="/" className="nav-brand">Trello Together</NavLink>
+                    <NavLink to="/" className="nav-brand">
+                        <img src={ttlogo} alt="Trello Together Logo" className="nav-logo" />
+                        <span>Trello Together</span>
+                    </NavLink>
                     <button 
                         className="hamburger-button"
                         onClick={() => setIsMenuOpen(!isMenuOpen)}
