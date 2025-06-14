dbd48f71845561129547c24f136c44c9602c695d
Brittany Joiner <brittanyjoiner15@gmail.com>
2025-06-13 21:28:19 -0500

    core bits of developers

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,24 @@
 import { NavLink, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function Navigation() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
         <nav className="main-nav">
             <div className="nav-container">
                 <div className="nav-content">
                     <NavLink to="/" className="nav-brand">Trello Together</NavLink>
-                    <ul className="nav-links">
+                    <button 
+                        className="hamburger-button"
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        aria-label="Toggle menu"
+                        aria-expanded={isMenuOpen}
+                    >
+                        <span className="hamburger-line"></span>
+                        <span className="hamburger-line"></span>
+                        <span className="hamburger-line"></span>
+                    </button>
+                    <ul className={`nav-links ${isMenuOpen ? 'nav-open' : ''}`}>
                         <li>
                             <NavLink
                                 to="/templates"
